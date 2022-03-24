c3f1afccfdc2bd1c9b05320cd6af4557e3d05845
ArkyvIO <arkyvio@gmail.com>
2022-03-23 21:39:39 -0400

    Added Lightmode
    Added light mode switch with saving variable to browser

---
+++ js/lightmode.js
@@ -0,0 +1,55 @@
+var lightmode = 'false';
+
+// Give toggle for switch
+const toggle = document.getElementById('lightModeSwitch');
+toggle.addEventListener('change', setLightMode, false);
+
+// Give r :root css
+const r = document.querySelector(':root');
+
+// Check for local storage key
+if (localStorage.getItem('lightmodeKey') === null) {
+    lightmode = 'false';
+    localStorage.setItem('lightmodeKey', 'false');
+} else {
+    lightmode = localStorage.getItem('lightmodeKey');
+}
+
+// Assign correct lightmode if not null
+if (lightmode === 'true') {
+    lightmode = 'false';
+    setLightMode();
+    toggle.checked = true;
+}
+
+function setLightMode() {
+    if (lightmode == 'false') {
+        lightmode = 'true';
+        localStorage.setItem("lightmodeKey", lightmode);
+        r.style.setProperty('--first-dark-transparent', '#d8dee9CC');
+        r.style.setProperty('--first-dark', '#d8dee9CC');
+        r.style.setProperty('--second-dark', '#e5e9f0');
+        r.style.setProperty('--third-dark', '#e5e9f0');
+        r.style.setProperty('--fourth-dark', '#eceff4');
+        r.style.setProperty('--first-light', '#2e3440');
+        r.style.setProperty('--second-light', '#3b4252');
+        r.style.setProperty('--third-light', '#434c5e');
+        r.style.setProperty('--background-dark', '#eceff4');
+        document.body.style.backgroundImage = "url('media/background2.jpg')";
+        document.body.style.backgroundSize = "cover";
+    } else if (lightmode == 'true') {
+        lightmode = 'false';
+        localStorage.setItem("lightmodeKey", lightmode);
+        r.style.setProperty('--first-dark-transparent', '2e3440cc');
+        r.style.setProperty('--first-dark', '#2e3440');
+        r.style.setProperty('--second-dark', '#3b4252');
+        r.style.setProperty('--third-dark', '#434c5e');
+        r.style.setProperty('--fourth-dark', '#4c566a');
+        r.style.setProperty('--first-light', '#d8dee9');
+        r.style.setProperty('--second-light', '#e5e9f0');
+        r.style.setProperty('--third-light', '#eceff4');
+        r.style.setProperty('--background-dark', '#1a1d24');
+        document.body.style.backgroundImage = "url('media/background.jpg')";
+        document.body.style.backgroundSize = "auto";
+    }
+}
--- js/load.js
+++ js/load.js
@@ -1,3 +1,11 @@
+// Set divs to hidden until loaded
+const contentBoxes = document.getElementsByClassName('content-box');
+
+// Set all content-boxes to transparent until full page loaded
+for (var i = 0; i < contentBoxes.length; i++) {
+    contentBoxes[i].classList.add('hidden');
+}
+
 // Only show the content boxes once the whol DOM has loaded
 window.addEventListener('DOMContentLoaded', function () {
     for (var i = 0; i < contentBoxes.length; i++) {
